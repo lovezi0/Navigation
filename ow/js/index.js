b56b75c7698c5ec5340b27f78bc4dab13fe953e1
张羊 <zhangy@hshbao.com>
2018-08-22 09:46:00 +0800

    add auto refreshContent add readme.txt

--- ow/js/index.js
+++ ow/js/index.js
@@ -5,12 +5,18 @@ layui.use([
     var $ = layui.$,
     layer = layui.layer;
 
-    $(function(){
-        //一言
-        $.getJSON("https://v1.hitokoto.cn/?v=f&encode=json",function(data){
-        	$(".content").text(data.hitokoto);
-          });
-    });
+    refreshContent();
+
+    setInterval(refreshContent,10000);
+
+    function refreshContent(){
+        $(function(){
+                //一言
+                $.getJSON("https://v1.hitokoto.cn/?v=f&encode=json",function(data){
+                	$(".content").text(data.hitokoto);
+                  });
+            });
+    }
 });
 
 
