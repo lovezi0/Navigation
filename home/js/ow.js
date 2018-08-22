aace281751af81cb3ccc487070ba36d412d876eb
张子凌 <lovezi0@hotmail.com>
2018-08-22 10:49:00 +0800

    add 404 page

--- home/js/ow.js
+++ home/js/ow.js
@@ -1,22 +1,19 @@
-layui.use([
-'layer',
-"jquery"
-], function(){
+layui.use(['layer', "jquery"],
+function() {
     var $ = layui.$,
     layer = layui.layer;
 
     refreshContent();
 
-    setInterval(refreshContent,10000);
+    setInterval(refreshContent, 10000);
 
-    function refreshContent(){
-        $(function(){
-                //一言
-                $.getJSON("https://v1.hitokoto.cn/?v=f&encode=json",function(data){
-                	$(".content").text(data.hitokoto);
-                  });
+    function refreshContent() {
+        $(function() {
+            //一言
+            $.getJSON("https://v1.hitokoto.cn/?v=f&encode=json",
+            function(data) {
+                $(".content").text(data.hitokoto);
             });
+        });
     }
 });
-
-
